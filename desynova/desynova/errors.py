113dbf55aee6046bb12480ee16c8569a5788ff67
kaynaat <nix.harry81@gmail.com>
2017-05-02 19:37:27 +0530

    added untracked files of web_scrapper which i forgot to add by mistake.

---
+++ desynova/desynova/errors.py
@@ -0,0 +1,5 @@
+
+STANDARD_ERROR_TEMPLATE = 'Error occured in processing this request. Detail is {0}'
+WRONG_AES_KEY_LENGTH = 'The Key length has to be 16, 24 or 32 bytes long. This key has {0} bytes in it'
+INVALID_KEY = 'The key you entered is invalid for this message'
+
---
+++ desynova/web_scrapper/views.py
@@ -0,0 +1,37 @@
+from django.core.cache import cache
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .constants import valid_data_types
+
+
+# Create your views here.
+
+
+class GainerLooserView(APIView):
+    """
+    The View which is hit by front end java script every x minute to get the gainer or loser data in json where x is defined
+    in .js file.
+    """
+    def get(self, request):
+        """
+        The get api fetches data stored in cache
+
+        :param request:
+        :return:
+        """
+        class_name = self.__class__.__name__
+        try:
+            data_type = request.query_params['type'] # gainer or loser
+            if not data_type in valid_data_types:
+                raise Exception('Invalid data type requested')
+
+            # tries to get data from the cache
+            cache_data = cache.get(data_type)
+            response = Response(data=cache_data, status=status.HTTP_200_OK)
+            response['Access-Control-Allow-Origin'] = '*'
+            return response
+        except Exception as e:
+            return Response(data=e.args or e.message, status=status.HTTP_400_BAD_REQUEST)
